1ca596fe461f9ec5284c09d3d4856e132b6000ec
ComradePetr <petr1996@bk.ru>
2012-11-18 01:17:07 +0400

    2

--- projection.cpp
+++ projection.cpp
@@ -19,12 +19,13 @@ int main(int argc, char *argv[]) {
 		point t=geographicalToRectangular(a+b/60,c+d/60);
 		c2[i].x=t.y, c2[i].y=-t.x; //http://upload.wikimedia.org/wikipedia/commons/4/44/Descartes_system_3D.png?uselang=ru
 	}
+	printf("%I64d %I64d %I64d %I64d ",left,right,top,bottom);
 	point c=c1[0]-c1[1], d=c2[0]-c2[1];
 	point v=point(c^d,c*d)/c.length2();
 	point st(c2[0].x-(c1[0]^v),c2[0].y-(c1[0]*v));
 
 	QImage in("input.jpg");
-	int W=right-left+1, H=bottom-top+1;
+	int W=in.width(), H=in.height();
 	QImage out(W,H,QImage::Format_RGB32);
 
 	LD MinX=INF, MinY=INF, MaxX=-INF, MaxY=-INF;
@@ -34,7 +35,7 @@ int main(int argc, char *argv[]) {
 			point B=st+point(A^v,A*v);
 			MinX=min(MinX,B.x), MaxX=max(MaxX,B.x), MinY=min(MinY,B.y), MaxY=max(MaxY,B.y);
 		}
-	LD Scale=min((W-1)/(MaxX-MinX),(H-1)/(MaxY-MinY));
+	LD Scale=min((right-left+1)/(MaxX-MinX),(bottom-top+1)/(MaxY-MinY));
 	forab(y,top,bottom+1)
 		forab(x,left,right+1){
 			point A(x,y);
