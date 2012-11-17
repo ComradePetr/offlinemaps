f4a244964d99c071494ebf796eb04785ef176345
ComradePetr <petr1996@bk.ru>
2012-11-18 01:18:44 +0400

    4

--- projection.cpp
+++ projection.cpp
@@ -1,24 +1,26 @@
-//19.10.12
+//18.11.12
 
 #include "all.h"
+#include "map-parser.h"
+#include "calc.h"
 
-extern point<LD> geographicalToRectangular(point<LD>);
-extern point<LD> rectangularToGeographical(point<LD>);
-extern LD degToRad(LD);
-extern pair<const char*,vector<pair<point<LD>,point<LD>>>> parse(const char *);
+typedef vector< pair< point<LD>,point<LD> > > vec_ldld;
 
+const LD INF=1e18,eps=0.08181333387657540019556205937723;
 
 point<LD> v,st;
-const LD INF=1e18,eps=0.08181333387657540019556205937723;
+LD zone;
 
 inline point<LD> Calculate(int x,int y){
 	static point<LD> A,B;
 	A=point<LD>(x,y);
 	B=st+point<LD>(A^v,A*v);
-	B=rectangularToGeographical(point<LD>(-B.y,B.x));
+	B=rectangularToGeographical(point<LD>(-B.y,B.x),zone);
 	B.x=degToRad(B.x), B.y=degToRad(B.y);
 	return point<LD>(B.y,arctanh(sin(B.x))-eps*arctanh(eps*sin(B.x)));
 }
+
+QImage *inImage,*outImage;
 
 int main(int argc, char *argv[]){
 	freopen("output.txt","w",stdout);
@@ -27,62 +29,55 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 	
-	auto parsed=parse(argv[1]);
-	auto ImageName=parsed.first;
-	auto input=parsed.second;
-	
+	pair<const char*,vec_ldld> parsed=parse(argv[1]);
+	const char *ImageName=parsed.first;
+	vec_ldld input=parsed.second;
 //	LL left=INF, right=-INF, top=INF, bottom=-INF;
 	point<LD> c1[2],c2[2];
+
 	for(int i=0;i<2;++i){
 		c1[i]=input[i].first;
+		if(!i)
+			zone=lonToZone(input[i].second.y);
 //		left=min(left,(LL)c1[i].x), right=max(right,(LL)c1[i].x), top=min(top,(LL)c1[i].y), bottom=max(bottom,(LL)c1[i].y);
-		point<LD> t=geographicalToRectangular(input[i].second);
+		point<LD> t=geographicalToRectangular(input[i].second,zone);
 		c2[i].x=t.y, c2[i].y=-t.x; //http://upload.wikimedia.org/wikipedia/commons/4/44/Descartes_system_3D.png?uselang=ru
 	}
 	point<LD> c=c1[0]-c1[1], d=c2[0]-c2[1];
 	v=point<LD>(c^d,c*d)/c.length2();
 	st=point<LD>(c2[0].x-(c1[0]^v),c2[0].y-(c1[0]*v));
 
-	QImage *in=new QImage(ImageName);
-	int W=in->width(), H=in->height();
+	inImage=new QImage(ImageName);
+	int W=inImage->width(), H=inImage->height();
 //	int W=right-left+1, H=bottom-top+1;
-	QImage *out=new QImage(W,H,QImage::Format_RGB32);
+	point<LD> *B=new point<LD>[W*H];
+	outImage=new QImage(W,H,QImage::Format_RGB32);
 
 	LD MinX=INF, MinY=INF, MaxX=-INF, MaxY=-INF;
-	point<LD> B;
 //	forab(y,top,bottom+1)
 //		forab(x,left,right+1){	
+	int pos=0;
 	forn(y,H)
 		forn(x,W){
-			B=Calculate(x,y);
-			MinX=min(MinX,B.x), MaxX=max(MaxX,B.x), MinY=min(MinY,B.y), MaxY=max(MaxY,B.y);
+			B[pos]=Calculate(x,y);
+			MinX=min(MinX,B[pos].x), MaxX=max(MaxX,B[pos].x);
+			MinY=min(MinY,B[pos].y), MaxY=max(MaxY,B[pos].y);
+			++pos;
 		}
 	LD Scale=min((W-1)/(MaxX-MinX),(H-1)/(MaxY-MinY));
-//	LD Scale=1;
 	LL a,b;
 //	forab(y,top,bottom+1)
 //		forab(x,left,right+1){	
+	pos=0;
 	forn(y,H)
 		forn(x,W){
-			B=Calculate(x,y);
-//			a=(B.x-MinX)*(W-1)/(MaxX-MinX), b=(MaxY-B.y)*(H-1)/(MaxY-MinY);
-			a=(B.x-MinX)*Scale, b=(MaxY-B.y)*Scale;
-			out->setPixel(a,b,in->pixel(x,y));
-		}		
-	
+			a=(B[pos].x-MinX)*Scale, b=(MaxY-B[pos].y)*Scale;
+			outImage->setPixel(a,b,inImage->pixel(x,y));
+			++pos;
+		}
+
 	strcpy(strrchr(ImageName,'.'),"-out.jpg");
-	out->save(ImageName);
+	outImage->save(ImageName);
 
-
-
-	QApplication *Application=new QApplication(argc,argv);
-
-	QMainWindow *MainForm = new QMainWindow(0, Qt::Window);
-	MainForm->resize(640, 480);
-	MainForm->setWindowTitle("Finished");
-	MainForm->show();
-
-	Application->exec();
-	
 	return 0;
 }
