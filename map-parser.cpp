0fe88f13914e7c4c5d92e23ae81e19ec606413ad
ComradePetr <petr1996@bk.ru>
2013-04-09 23:00:55 +0400

    Pretty

--- map-parser.cpp
+++ map-parser.cpp
@@ -12,7 +12,7 @@ pair<const char*,parseOut> parse(const char *FileName){
 	char (*S)[LEN]=new char[FIRST_STRS][LEN];
 	forn(i,FIRST_STRS)
 		fgets(S[i],LEN,file);
-	parseOut answer;		
+	parseOut answer;
 	forn(i,POINTS_CNT){
 		int x,y,latdeg,londeg;
 		double latmin,lonmin;
@@ -20,14 +20,15 @@ pair<const char*,parseOut> parse(const char *FileName){
 
 		if(fscanf(file,"%*[^,],%*s %d,%d,%*[^,],%*[^,],%d,%lf,%c,%d,%lf,%c%*[^\n]\n",&x,&y,&latdeg,&latmin,&latc,&londeg,&lonmin,&lonc)!=8)
 			break;
-		
+
 		LD lat=latdeg+latmin/60, lon=londeg+lonmin/60;
-//		lat*=(latc=='N'?+1:-1), lon*=(lonc=='E'?+1:-1);		
+		lat*=(latc=='N'?+1:-1), lon*=(lonc=='E'?+1:-1);
 		answer.pb(mp(point<int>(x,y),point<LD>(lat,lon)));
 	}
-	
+
 	int L=strlen(S[2]);
 	while(L && isspace(S[2][L-1]))
 		S[2][L-1]=0, --L;
+	fclose(file);
 	return mp(S[2],answer);
 }
--- projection.cpp
+++ projection.cpp
@@ -9,34 +9,46 @@ using namespace alglib;
 #include <sys/stat.h>
 #include <unistd.h>
 
-const LD INF=1e18,eps=0.08181333387657540019556205937723;
+const LD
+	INF=1e18,
+	eps=0.08181333387657540019556205937723;
 
 point<LD> v,st;
-
-QImage *inImage,*outImage;
 
 real_1d_array X,Y;
 real_2d_array Z;
 
 const int
 	CFF[2][4]={{1,0,-1,1},{0,1,1,1}},
-	ITERS_CNT=2;
+	ITERS_CNT=20,
+	T_SIZE=256;
+const double
+	ScaleCoeff=5e4;
+
+inline void Error(const char *S="Error"){
+	fputs(S,stderr);
+	exit(1);
+}
+inline void Print(const char *S="Test"){
+	fputs(S,stderr);
+	fflush(stderr);
+}
+
+QImage *inImage;
 
 int main(int argc, char *argv[]){
-	freopen("output.txt","w",stdout);
-	if(argc==1){
-		puts("USAGE: project.exe name-of-map-file.map");
-		return 0;
-	}
+	freopen("error.log","w",stderr);
+
+	if(argc==1)
+		Error("USAGE: project.exe name-of-map-file.map");
 
 	pair<const char*,parseOut> parsed=parse(argv[1]);
+
 	const char *ImageName=parsed.first;
 	parseOut input=parsed.second;
 	int L=input.size();
-	if(L<=1){
-		puts("Not enough points");
-		return 0;
-	}
+	if(L<=1)
+		Error("Not enough points");
 
 	Y.setlength(2*L), Z.setlength(2*L,4);
 	LD zone=lonToZone(input[0].snd.y);
@@ -53,29 +65,32 @@ int main(int argc, char *argv[]){
 	ae_int_t info;
 	lsfitreport rep;
 	lsfitlinear(Y,Z,info,X,rep);
-	if(info!=1){
-		puts("Can't solve matrix");
-		return 0;
-	}
+	if(info!=1)
+		Error("Can't solve matrix");
+
 	st=point<LD>(X[0],X[1]), v=point<LD>(X[2],X[3]);
 
 	struct stat imstat;
 	stat(ImageName,&imstat);
 	int sz=imstat.st_size;
+
 	FILE *imageFile=fopen(ImageName,"rb");
+	if(!imageFile)
+		Error("Can't open file with image");
 	uchar *buff=new uchar[sz];
 	fread(buff,sizeof(uchar),sz,imageFile);
 	fclose(imageFile);
 
+	Print("stamp0");
 	inImage=new QImage();
+	Print("stamp1");
 	inImage->loadFromData(buff,sz);
-	if(inImage->isNull()){
-		puts("Can't open image");
-		return 0;
-	}
-
+	Print("stamp2");
+	if(inImage->isNull())
+		Error("Can't open image");
+	Print("stamp3");
 	int W=inImage->width(), H=inImage->height();
-	outImage=new QImage(W,H,QImage::Format_RGB32);
+	Print("stamp4");
 
 	LD MinX=INF, MinY=INF, MaxX=-INF, MaxY=-INF;
 	point<LD> t;
@@ -92,16 +107,18 @@ int main(int argc, char *argv[]){
 			MinY=min(MinY,t.y), MaxY=max(MaxY,t.y);
 		}
 
-
 	point<LD> v_rev=point<LD>(-v.x,v.y)/v.length2();
 	LD Scale=min((W-1)/(MaxX-MinX),(H-1)/(MaxY-MinY)),yt,theta;
+	int W1=int(Scale*(MaxX-MinX))+1,H1=int(Scale*(MaxY-MinY))+1;
+	QImage *outImage=new QImage(W1,H1,QImage::Format_RGB32);
 	int ix,iy;
-	forn(y,H)
-		forn(x,W){
-			yt=MaxY-y/Scale, theta=atan(sinh(yt));
+	forn(y,H1/3)
+		forn(x,W1/3){
+			yt=MaxY-y*3/Scale, theta=atan(sinh(abs(yt)));
 			forn(i,ITERS_CNT)
-				theta=asin(1-(1+sin(theta))*pow(1-eps*sin(theta),eps)/(exp(2*yt)*pow(1+eps*sin(theta),eps)));
-			t=point<LD>(theta,MinX+x/Scale);
+				theta=asin(1-(1+sin(theta))*pow(1-eps*sin(theta),eps)/(exp(2*abs(yt))*pow(1+eps*sin(theta),eps)));
+			theta*=sign(yt);
+			t=point<LD>(theta,MinX+x*3/Scale);
 			forn(i,2)
 				t[i]=radToDeg(t[i]);
 			t=geographicalToRectangular(t,zone);
@@ -109,17 +126,87 @@ int main(int argc, char *argv[]){
 			t=t-st;
 			t=point<LD>(t^v_rev,t*v_rev);
 			ix=(int)t.x, iy=(int)t.y;
-			if(inImage->valid(ix,iy))
-				outImage->setPixel(x,y,inImage->pixel(ix,iy));
+			for(int ix1=-1;ix1<2;ix1++)
+				for(int iy1=-1;iy1<2;iy1++)
+					if(inImage->valid(ix+ix1,iy+iy1))
+						outImage->setPixel(x*3+ix1,y*3+iy1,inImage->pixel(ix+ix1,iy+iy1));
 		}
+	double dx=(MaxX-MinX),dy=(MaxY-MinY);
+	int W2=(int)(dx*ScaleCoeff),H2=(int)(dy*ScaleCoeff);
+	QImage *outImageScaled=new QImage(W2,H2,QImage::Format_RGB32);
+	forn(y,H2)
+		forn(x,W2){
+			int x1=(int)((x/ScaleCoeff)*Scale);
+			int y1=(int)((y/ScaleCoeff)*Scale);
+			if(outImage->valid(x1,y1))
+				outImageScaled->setPixel(x,y,outImage->pixel(x1,y1));
+		}
+	int nx=(int)W2/T_SIZE+1,ny=(int)H2/T_SIZE+1;
+	QImage *Tile[nx][ny];
+	forn(i,nx)
+		forn(j,ny)
+			Tile[i][j]=new QImage(T_SIZE,T_SIZE,QImage::Format_RGB32);
+	forn(j,ny)
+		forn(i,nx)
+			forn(y,T_SIZE)
+				forn(x,T_SIZE){
+					ix=x+i*T_SIZE;
+					iy=y+j*T_SIZE;
+					if(outImageScaled->valid(ix,iy))
+						Tile[i][j]->setPixel(x,y,outImageScaled->pixel(ix,iy));
+				}
 
 	srand(time(NULL));
 	char TempName[30];
 	sprintf(TempName,"temp%d.jpg",rand());
 	strcpy(strrchr(ImageName,'.'),"-out.jpg");
-	outImage->save(TempName);
-	remove(ImageName);
-	rename(TempName,ImageName);
+//	outImage->save(TempName);
+//	remove(ImageName);
+//	rename(TempName,ImageName);
+	outImage->save(ImageName);
+	strcpy(strrchr(ImageName,'.'),"-scaled.jpg");
+	outImageScaled->save(ImageName);
+	strcpy(strrchr(ImageName,'.'),"-");
+	char ImageName1[200];
+	forn(i,nx)
+		forn(j,ny){
+			sprintf(ImageName1,"%s%d-%d.jpg",ImageName,i+1,j+1);
+			Tile[i][j]->save(ImageName1);
+		}
+
+
+	strcpy(strrchr(argv[1],'.'),"-out.map");
+	freopen(argv[1],"w",stdout);
+
+	printf("OziExplorer Map Data File Version 2.2\n");
+	printf("%s\n%s\n",ImageName,ImageName);
+	puts("1 ,Map Code,\n\
+Pulkovo 1942 (2),WGS 84,   0.0000,   0.0000,WGS 84\n\
+Reserved 1\n\
+Reserved 2\n\
+Magnetic Variation,,,E\n\
+Map Projection,Mercator,PolyCal,No,AutoCalOnly,No,BSBUseWPX,No\n");
+
+	forn(i,L){
+		t=point<LD>(input[i].fst.x,input[i].fst.y);
+		t=st+point<LD>(t^v,t*v);
+		t.cartToGeo();
+		t=rectangularToGeographical(t,zone);
+		forn(j,2)
+			t[j]=degToRad(t[j]);
+		t=point<LD>(t.y,arctanh(sin(t.x))-eps*arctanh(eps*sin(t.x)));
+		t=point<LD>((t.x-MinX)*Scale,(MaxY-t.y)*Scale);
+		ix=(int)t.x,iy=(int)t.y;
+		ix=(int)(ix/Scale*ScaleCoeff);
+		iy=(int)(iy/Scale*ScaleCoeff);
+		printf("Point%02d,xy,   %d,   %d,in, deg,  %d, %.10lf,%c, %d, %.10lf,%c, grid,   ,           ,           ,N\n",i+1,ix,iy,
+			(int)abs(input[i].snd.x),(double)(abs(input[i].snd.x)-(int)abs(input[i].snd.x)),input[i].snd.x>0?'N':'S',
+			(int)abs(input[i].snd.y),(double)(abs(input[i].snd.y)-(int)abs(input[i].snd.y)),input[i].snd.y>0?'W':'E');
+	}
+	forab(i,L,30)
+		printf("Point%02d,xy,     ,     ,in, deg,    ,        ,,    ,        ,, grid,   ,           ,           ,",i+1);
+
+	fclose(stdout);
 
 	return 0;
 }
